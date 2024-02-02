54212cf823e72e7651b5b9eb1f0d87e737e4d487
akshaymehtastudio88 <148732672+akshaymehtastudio88@users.noreply.github.com>
2024-02-02 16:38:51 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,53 @@
+function demo() {
+    var x = document.getElementById("ans1");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function demo1() {
+    var x = document.getElementById("ans2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function demo2() {
+    var x = document.getElementById("ans3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function demo3() {
+    var x = document.getElementById("ans4");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function demo4() {
+    var x = document.getElementById("ans5");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function demo5() {
+    var x = document.getElementById("ans6");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
